ca6be44857157a2b4dc9261f769f5efae24ea723
Coman Cosmin <cosmin.coman@middleout.com>
2021-12-02 12:21:48 +0200

    Part 2 done fro day one

--- day1/day1.js
+++ day1/day1.js
@@ -1,5 +1,8 @@
 fs = require("fs");
 
+/*
+ * PART 1
+ */
 let count = 0;
 const reducer = (previousValue, currentValue) => {
     previousValue < currentValue ? count++ : 0;
@@ -13,4 +16,21 @@ fs.readFile("./input.txt", "utf8", function (err, data){
     splitCoordinates.reduce(reducer);
 
     console.log(count);
+})
+
+/*
+ * PART 2
+ */
+fs.readFile("./input2.txt", "utf8", function (err, data){
+    let localCount = 0;
+    const splitCoordinates = Array.from(data.split('\n'), x => parseInt(x));
+
+    for(let i = 3; i < splitCoordinates.length; i++){
+        const firstSum = splitCoordinates[i-3] + splitCoordinates[i-2] + splitCoordinates[i-1];
+        const secondSum = splitCoordinates[i-2] + splitCoordinates[i-1] + splitCoordinates[i];
+
+        if(firstSum < secondSum) localCount++;
+    }
+
+    console.log(localCount);
 })
