4d31448a62c3aad85162bb11154804a981f6e66b
B̴l̷i̷t̶x̷ <59416533+Blitzcryp@users.noreply.github.com>
2021-12-06 20:04:07 +0200

    well fuck you part 2

--- day5/day5.js
+++ day5/day5.js
@@ -43,7 +43,7 @@ fs.readFile("./input.txt", "utf8", function (err, data){
 });
 
 // Part 2
-fs.readFile("./test2.txt", "utf8", function (err, data){
+fs.readFile("./input2.txt", "utf8", function (err, data){
     let result = Array.from(data.split('\n'));
     result = result.map( (coordinates) => {
         const newCoords =  coordinates.split("->");
@@ -72,26 +72,24 @@ fs.readFile("./test2.txt", "utf8", function (err, data){
                 const point = y1.toString() + '-' + i.toString();
                 collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
             }
-        } else {
-            let point = x1.toString() + '-' + y1.toString();
-            collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
-
-            point = x2.toString() + '-' + y2.toString();
-            collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
-
+        }
+        if(x1 !== x2  && y1 !== y2) {
+            console.log({x1, y1, x2, y2});
             while (x1 !== x2 && y1 !== y2){
+                point = x1.toString() + '-' + y1.toString();
+
+                console.log({point});
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+
                 x1 < x2 ? x1++ : x1--;
                 y1 < y2 ? y1++ : y1--;
-
-                point = x1.toString() + '-' + y1.toString();
-                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
             }
+            console.log("---");
         }
     });
 
     let sum = 0;
     for(const [key, value] of Object.entries(collectionOfPoints)){
-        console.log({key, value});
         if(value >= 2){
             sum++;
         }
