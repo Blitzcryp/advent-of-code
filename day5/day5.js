f1a88898a75d51d5aa0c27189115670700abf6a0
B̴l̷i̷t̶x̷ <59416533+Blitzcryp@users.noreply.github.com>
2021-12-06 19:53:33 +0200

    Fuck me twice

--- day5/day5.js
+++ day5/day5.js
@@ -1,6 +1,6 @@
 fs = require("fs");
 
-
+// Part 1
 fs.readFile("./input.txt", "utf8", function (err, data){
     let result = Array.from(data.split('\n'));
     result = result.map( (coordinates) => {
@@ -17,73 +17,85 @@ fs.readFile("./input.txt", "utf8", function (err, data){
         };
     });
 
-    const allCoordinates = {};
-
-    let pointsPassingThrough = result.map( ({ x1, y1, x2, y2 }) => {
-        let generatedPoints = [];
-
+    let collectionOfPoints = {};
+    result.forEach( ({ x1, y1, x2, y2 }) => {
         if(x1 === x2){
             for(let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++){
-                generatedPoints.push([i, x1]);
+                const point = i.toString() + '-' + x1.toString();
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
             }
         }
 
         if(y1 === y2){
             for(let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++){
-                generatedPoints.push([y1, i]);
+                const point = y1.toString() + '-' + i.toString();
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
             }
         }
-        return generatedPoints;
     });
-
-
-    let maxPoint = 0;
-    pointsPassingThrough.forEach( ( element ) => {
-        element.forEach( ([p1, p2]) => {
-            p1 > maxPoint ? maxPoint = p1 : p2 > maxPoint ? maxPoint = p2 : maxPoint;
-        })
-    });
-    console.log(maxPoint);
-
-
-    let test = [];
-    let chunkToPush = [];
-    for(let i = 0; i <= maxPoint; i++){
-        chunkToPush.push(".");
-    }
-
-    for(let i = 0; i < pointsPassingThrough.length; i++){
-        test.push([...chunkToPush]);
-    }
-
-    console.log(test[57][500]);
-
-    pointsPassingThrough.forEach( (pointsGroup) => {
-        pointsGroup.forEach( ([x, y]) => {
-            test[x][y] === "." ?
-                test[x][y] = 1 :
-                test[x][y] += 1;
-        } )
-
-    });
-
 
     let sum = 0;
-    test.forEach((row) => {
-        row.forEach( (element) => {
-            if( element !== "." && element >= 2){
-                sum += 1;
-            }
-        })
-    });
-
-    console.log({ sum });
-
-    displayTest(test);
+    for(const [key, value] of Object.entries(collectionOfPoints)){
+        if(value >= 2){
+            sum++;
+        }
+    }
 });
 
-function displayTest(test){
-    test.forEach( (line) => {
-        console.log(...line);
-    })
-}
+// Part 2
+fs.readFile("./test2.txt", "utf8", function (err, data){
+    let result = Array.from(data.split('\n'));
+    result = result.map( (coordinates) => {
+        const newCoords =  coordinates.split("->");
+        const [x1, y1] = newCoords[0].split(",");
+        const [x2, y2] = newCoords[1].split(",");
+
+
+        return {
+            x1: parseInt(x1),
+            y1: parseInt(y1),
+            x2: parseInt(x2),
+            y2: parseInt(y2)
+        };
+    });
+
+    let collectionOfPoints = {};
+    result.forEach( ({ x1, y1, x2, y2 }) => {
+        if(x1 === x2){
+            for(let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++){
+                const point = i.toString() + '-' + x1.toString();
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+            }
+        }
+        if(y1 === y2){
+            for(let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++){
+                const point = y1.toString() + '-' + i.toString();
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+            }
+        } else {
+            let point = x1.toString() + '-' + y1.toString();
+            collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+
+            point = x2.toString() + '-' + y2.toString();
+            collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+
+            while (x1 !== x2 && y1 !== y2){
+                x1 < x2 ? x1++ : x1--;
+                y1 < y2 ? y1++ : y1--;
+
+                point = x1.toString() + '-' + y1.toString();
+                collectionOfPoints[point] !== undefined ? collectionOfPoints[point] += 1 : collectionOfPoints[point] = 1;
+            }
+        }
+    });
+
+    let sum = 0;
+    for(const [key, value] of Object.entries(collectionOfPoints)){
+        console.log({key, value});
+        if(value >= 2){
+            sum++;
+        }
+    }
+
+    console.log({sum});
+});
